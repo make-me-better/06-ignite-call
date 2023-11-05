9f8df27f21192db8267669de276023f8683e1f65
Fernando Lara Amaral <37773885+fernandolaraamaral@users.noreply.github.com>
2023-11-05 20:12:36 -0300

    .

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,34 @@
 import { Heading, Text } from '@ignite-ui/react'
 import { Container, Hero, Preview } from './styles'
+import Image from 'next/image'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+
+import previewImage from '../../assets/app-preview.png'
 
 export default function Home() {
   return (
     <Container>
       <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </Text>
+
+        <ClaimUsernameForm />
       </Hero>
 
-      <Preview></Preview>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
     </Container>
   )
 }
